7a1a4b46166544931cd295311e13b37ec8d44d8d
Joshua Fairchild and Tyson Stuart <joshua@>
2017-02-13 20:51:44 -0800

    king shit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,82 @@
-var add = function(peanutbutter, jellytime){
-  return peanutbutter + jellytime;
-};
+  var add = function(number1, number2){
+    return number1 + number2;
+  };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
 
-alert(add(number1, number2));
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
+$(function() {
+  $("form#add").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (add(number1, number2));
+    $("#result-add").text(result);
+
+    event.preventDefault();
+  });
+
+  $("form#subtract").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (subtract(number1, number2));
+    $("#result-subtract").text(result);
+
+    event.preventDefault();
+  });
+
+  $("form#multiply").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (multiply(number1, number2));
+    $("#result-multiply").text(result);
+
+    event.preventDefault();
+  });
+
+  $("form#divide").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (divide(number1, number2));
+    $("#result-divide").text(result);
+
+    event.preventDefault();
+  });
+
+  $("#Addition").click(function() {
+    $("#subtract").hide();
+    $("#multiply").hide();
+    $("#divide").hide();
+    $("#add").show();
+  });
+
+  $("#Subtraction").click(function() {
+    $("#subtract").show();
+    $("#multiply").hide();
+    $("#divide").hide();
+    $("#add").hide();
+  });
+
+  $("#Multiplication").click(function() {
+    $("#subtract").hide();
+    $("#divide").hide();
+    $("#add").hide();
+    $("#multiply").show();
+  });
+
+  $("#Division").click(function() {
+    $("#subtract").hide();
+    $("#multiply").hide();
+    $("#add").hide();
+    $("#divide").show();
+  });
+
+});
